ec98f6e31796726246c75d71a2cee0af2ead8ed2
Matthew <matthew.sanford@rebilly.com>
2024-04-05 12:32:31 -0400

    update

--- README.md
+++ README.md
@@ -9,21 +9,22 @@ Clone the repo and run `yarn install`, then copy the `.env` file and define your
 ### Environment Variables
 
 ```bash
-TARGET_URL=https://demo.acme.test.com
-API_KEY=sk_sandbox_0000000000000000000000000000
 API_URL=https://api-sandbox.rebilly.com
+API_KEY=sk_sandbox_0000000000000000000000000000
+ORGANIZATION_ID=org_0000000000000000000000000000
+WEBSITE_ID=web_0000000000000000000000000000
 ```
 
 ### Usage
 
-To trigger a token and open in the browser run 
+To trigger a token and open in the browser run
 
 ```bash
 yarn go -c <customerId>
 ```
 
-The default redirect is Google but can be overwritten with `-r URL`.
+Change the permissions with `-p` flag. Available permissions are visible within `permissions.js`. Default permissions is `admin`
 
 ```bash
-yarn go -c <customerId> -r https://amazon.com
-```
+yarn go -c <customerId> -p <permission key>
+``
--- index.js
+++ index.js
@@ -1,56 +1,57 @@
 const RebillyAPI = require('rebilly-js-sdk').default;
 const chalk = require('chalk');
 const program = require('commander');
-const permissions = require('./permissions');
+const allPermissions = require('./permissions');
 
 program
-    .option('-p, --permissions <permission_key>', 'which permissions to use')
-    .option('-c, --customerId <id>', 'customer ID for which to create token')
-    .option('-v, --verbose', 'verbose');
+    .option('-p, --permission <permission_key>', 'which permission preset to use', 'admin')
+    .option('-s, --permissions <permissions_array>', 'array of permission operation keys')
+    .option('-c, --customerId <id>', 'customer ID for which to create token');
 
 program.parse(process.argv);
 
 const {API_URL = null, API_KEY = null, ORGANIZATION_ID = null, WEBSITE_ID = null} = process.env;
-const log = console.log;
 
-let permissionValues = null;
-const permissionKey = program.permissions ?? 'admin';
-if (permissions[permissionKey]) {
-    permissionValues = permissions[permissionKey];
+let permissions = null;
+if (!program.permissions && allPermissions[program.permission]) {
+    permissions = allPermissions[program.permission];
+} else if (program.permissions) {
+    console.log(program.permissions);
+    permissions = program.permissions.split(',').map(v => v.trim());
 } else {
-    return log(chalk.red(`"${program.permissions}" is not a supported permission`));
+    return console.log(chalk.red(`"${program.permissions}" is not a supported permission`));
 }
 
-if (program.verbose) {
-    console.log({
-        API_URL,
-        API_KEY,
-        ORGANIZATION_ID,
-        WEBSITE_ID,
-        customerId: program.customerId,
-        permissions: permissionValues
-    });
-}
+const announce = (title) => {
+    console.log(chalk.bold.cyan(`${title}\n`));
+};
+
+announce('Configuration');
+console.log({
+    API_URL,
+    API_KEY,
+    ORGANIZATION_ID,
+    WEBSITE_ID,
+    customerId: program.customerId,
+    permissions,
+}, '\n\n');
 
 const sandbox = API_KEY.includes('_sandbox');
 const api = RebillyAPI({apiKey: API_KEY, sandbox});
 const endpoint = API_URL ? API_URL : 'http://api-sandbox.dev-local.rebilly.com';
 api.setEndpoints({[sandbox ? 'sandbox' : 'live']: endpoint});
 
-const announce = (title) => {
-    log(chalk.bold.cyan(title));
-};
-
 (async () => {
     try {
-        announce('Creating login token...\n');
+        announce('Creating login token...');
         const data = {
             mode: 'passwordless',
             customerId: program.customerId,
         };
         const {fields: {token}} = await api.customerAuthentication.login({data});
-        log(chalk.yellow(token));
-        announce('\n\nExchanging login for access token...\n');
+        console.log(chalk.yellow(token), '\n\n');
+
+        announce('Exchanging login for access token...');
         const {fields: {token: jwt}} = await api.customerAuthentication.exchangeToken({
             token,
             data: {
@@ -59,15 +60,16 @@ const announce = (title) => {
                     scope: {
                         organizationId: [ORGANIZATION_ID],
                     },
-                    permissions: permissionValues,
+                    permissions,
                 }],
                 customClaims: {
                     websiteId: WEBSITE_ID
                 }
             }
         });
-        log(chalk.green(jwt));
+        console.log(chalk.green(jwt));
     } catch (err) {
-        return log(chalk.red(err));
+        console.log(chalk.red(`${err}\n`));
+        return console.log(err);
     }
 })();
